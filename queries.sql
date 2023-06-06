f3ce341daa0f323af6c9d4539173edfdaf54821b
Alfie Yang <yanminthwin@gmail.com>
2023-06-06 23:41:53 +0630

    Add new queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,25 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+select * from animals where RIGHT(nam
+e,3) = 'mon';
+
+select * from animals
+where date_of_birth > '2016-01-01' and date_of_birth < '2019-01-01';
+
+select name from animals
+where neutered and escape_attempts < 3;
+
+select date_of_birth from animals
+where name = 'Agumon' or name='Pikachu';
+
+select name and escape_attempts from animals
+where weight_kg > 10.5;
+
+select * from animals
+where neutered;
+
+select * from animals
+where name != 'Gabumon';
+
+select * from animals
+where weight_kg >= 10.4 and weight_kg <= 17.3;
