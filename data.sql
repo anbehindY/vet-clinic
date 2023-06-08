56850aa4a7b6a1cc48777df22a3065b4343c13fe
Alfie Yang <yanminthwin@gmail.com>
2023-06-08 23:03:53 +0630

    Update data.sql

--- data.sql
+++ data.sql
@@ -12,3 +12,40 @@ VALUES
 ('Boarmon','2005-06-07',7,true,20.4),
 ('Blossom','1998-10-13',3,true,17),
 ('Ditto','2022-05-14',4,true,22);
+
+insert into owners(full_name, age)
+values('Sam Smith', 34),('Jennifer Orwell', 19),
+('Bob', 45), ('Melody Pond', 77), 
+('Dean Winchester', 14), ('Jodie Whittaker', 48);
+
+insert into species (name)
+values('Pokemon'), ('Digimon');
+
+update animals
+set species_id = 1
+where species_id is null;
+
+update animals
+set species_id = 2 
+where right(name,3)='mon';
+
+update animals
+set owner_id = 1
+where name='Agumon';
+
+update animals
+set owner_id = 2
+where name='Gabumon' or name='Pikachu';
+
+update animals
+set owner_id = 3
+where name='Devimon' or name='Plantmon';
+
+update animals
+set owner_id = 4
+where name='Charmander' or name='Squirtle' or name='Blossom';
+
+update animals
+set owner_id = 5
+where name='Angemon' or name='Boarmon';
+select * from animals;
