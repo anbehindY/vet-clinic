b186808864934d5a8672e15bc7bcb63a5f7c73b8
Alfie Yang <yanminthwin@gmail.com>
2023-06-09 22:50:01 +0630

    Added link tables

--- schema.sql
+++ schema.sql
@@ -37,3 +37,27 @@ alter table animals
 add column owner_id int
 constraint fk_owner_id
 references owners(id);
+
+create table vets(
+	id int generated always as identity,
+	name varchar(20),
+	age int,
+	date_of_graduation date,
+	primary key(id)
+);
+
+create table specializations (
+	species_id int,
+	vet_id int,
+	primary key(species_id, vet_id),
+	foreign key(species_id) references species(id),
+	foreign key(vet_id) references vets(id)
+);
+
+create table visits (
+	animal_id int,
+	vet_id int,
+	primary key(animal_id, vet_id),
+	foreign key(animal_id) references animals(id),
+	foreign key(vet_id) references vets(id)
+);
